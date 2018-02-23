bdb7fc139c0ca1d0d4ac50c23beb17621bd2f8a6
Your Name <example@example.com>
2018-02-23 21:25:01 +0200

    update findviewbyid to be called once and use several

--- app/src/main/java/com/example/android/courtcounterapp/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounterapp/MainActivity.java
@@ -6,56 +6,83 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int goalForRm = 0;
-    int foulForRm = 0;
-    int yellowCardForRm = 0;
-    int redCardForRm = 0;
+    private int goalForRm ;
+    private int foulForRm ;
+    private int yellowCardForRm ;
+    private int redCardForRm ;
 
-    int goalForB = 0;
-    int foulForB = 0;
-    int yellowCardForB = 0;
-    int redCardForB = 0;
+    private int goalForB ;
+    private int foulForB ;
+    private int yellowCardForB ;
+    private int redCardForB ;
 
+    TextView scoreView;
+    TextView foulView;
+    TextView cardView ;
+    TextView RcardView ;
+    TextView goalView_b ;
+    TextView foulView_b ;
+    TextView cardView_b ;
+    TextView red_b ;
+    private static final int default_value = 0;
     // display methods for real madrid
 
+
+    // onCreate method
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scoreView = (TextView) findViewById(R.id.goalRM);
+        foulView = (TextView) findViewById(R.id.foulRM);
+        cardView = (TextView) findViewById(R.id.ycardRM);
+        RcardView = (TextView) findViewById(R.id.rcardRM);
+        goalView_b = (TextView) findViewById(R.id.goalB);
+        foulView_b = (TextView) findViewById(R.id.foulB);
+       cardView_b = (TextView) findViewById(R.id.ycardB);
+       red_b = (TextView) findViewById(R.id.rcardB);
+    }
+
+
     public void displayGoalRM(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.goalRM);
+
         scoreView.setText(String.valueOf(score));
     }
 
     public void displayFoulRM(int foul_r) {
-        TextView foulView = (TextView) findViewById(R.id.foulRM);
+
         foulView.setText(String.valueOf(foul_r));
     }
 
     public void displayCardRM(int card_r) {
-        TextView cardView = (TextView) findViewById(R.id.ycardRM);
+
         cardView.setText(String.valueOf(card_r));
     }
 
     public void displayRcardRM(int rcard_r) {
-        TextView RcardView = (TextView) findViewById(R.id.rcardRM);
+
         RcardView.setText(String.valueOf(rcard_r));
     }
 
     //display methods for barcelona
     public void displayGoalB(int goal_b) {
-        TextView goalView_b = (TextView) findViewById(R.id.goalB);
+
         goalView_b.setText(String.valueOf(goal_b));
     }
 
     public void displayFoulB(int foul_b) {
-        TextView foulView_b = (TextView) findViewById(R.id.foulB);
+
         foulView_b.setText(String.valueOf(foul_b));
     }
 
     public void displayCardB(int card_b) {
-        TextView cardView_b = (TextView) findViewById(R.id.ycardB);
+
         cardView_b.setText(String.valueOf(card_b));
     }
 
     public void displayRcardB(int rcard_b) {
-        TextView red_b = (TextView) findViewById(R.id.rcardB);
+
         red_b.setText(String.valueOf(rcard_b));
     }
 
@@ -105,27 +132,22 @@ public class MainActivity extends AppCompatActivity {
         displayCardB(yellowCardForB);
     }
 
-    // onCreate method
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
 
 
 
 // reset method
 
+
     public void reset(View v) {
-        goalForRm = 0;
-        foulForRm = 0;
-        yellowCardForRm = 0;
-        redCardForRm = 0;
-        redCardForB = 0;
-        yellowCardForB = 0;
-        foulForB = 0;
-        goalForB = 0;
+
+        goalForRm = default_value;
+        foulForRm = default_value;
+        yellowCardForRm = default_value;
+        redCardForRm = default_value;
+        redCardForB = default_value;
+        yellowCardForB = default_value;
+        foulForB = default_value;
+        goalForB = default_value;
         displayGoalRM(goalForRm);
         displayFoulRM(foulForRm);
         displayCardRM(yellowCardForRm);
